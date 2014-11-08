7129292dfd6ae9b0f23b91e54a9541206a6cb042
AragakiAyase <83922819@qq.com>
2014-11-08 14:52:08 +0800

    Version 1.0: basic function accomplished

--- expression.cpp
+++ expression.cpp
@@ -2,7 +2,27 @@
 #include <vector>  //std::vector
 #include <stack>   //std::stack
 #include <cctype>  //isdigit()
+#include <cstdlib> //stod()
+#include <map>     //std::map
 #include <iostream>
+
+std::string version("1.0.0");
+
+//declaration of operation function
+double plus(double, double);
+double subtract(double, double);
+double multiple(double, double);
+double divide(double, double);
+double parallel(double, double);
+
+std::map<char, double(*)(double, double)> oper_map
+{
+	std::make_pair('+', plus),
+	std::make_pair('-', subtract),
+	std::make_pair('*', multiple),
+	std::make_pair('/', divide),
+	std::make_pair('|', parallel)
+};
 
 int comparePiority(const char lhs, const char rhs); // lhs>rhs returns 1, lhs=rhs returns 0, lhs<rhs return -1
 int getPiority(const char oper);
@@ -99,11 +119,86 @@ int getPiority(const char oper)
 	}
 }
 
+double evalrpn(const std::string &rpn)
+{
+	std::stack<double> stk;
+	for (auto iter = rpn.begin();
+		iter != rpn.end();
+		/*Empty*/)
+	{ 
+		if (std::isdigit(*iter))
+		{
+			auto beg = iter;			
+			while (*iter != ' ') //make iter reaching spacing char
+			{
+				++iter;
+			}
+			stk.push(std::stod(rpn.substr(beg - rpn.begin())));
+			++iter; //points to the next
+		}
+		else //the iter points to an operator
+		{
+			if (stk.size() > 1)
+			{
+				double lhs = stk.top();
+				stk.pop();
+				double rhs = stk.top();
+				stk.pop();
+				stk.push(oper_map.find(*iter)->second(lhs, rhs));
+			}
+			else //something goes wrong
+			{
+				throw std::runtime_error("The expression is illegl");
+			}
+			++iter;//to skip spacing char
+			++iter; 
+		}
+	}
+	return stk.top(); 
+}
+
+double plus(double lhs, double rhs)
+{
+	return lhs + rhs;
+}
+
+double subtract(double lhs, double rhs)
+{
+	return lhs - rhs;
+}
+
+double multiple(double lhs, double rhs)
+{
+	return lhs*rhs;
+}
+
+double divide(double lhs, double rhs)
+{
+	return lhs / rhs;
+}
+
+double parallel(double lhs, double rhs)
+{
+	return (1 / ((1 / lhs) + (1 / rhs)));
+}
+
 int main()
 {
-
-	std::string str;
-	std::cin>>str;
-	std::cout<<infixtorpn(str);
-	return 0;
+	std::string input;
+	std::cout << "Circuit Calculator Interactive Mode" << std::endl
+		<< "Version: " << version << std::endl
+		<< "Programmed by AragakiAyase in 2014."
+		<< "Push Ctrl-Z to terminate the program" << std::endl;
+	while (std::cin >> input)
+	{
+		try
+		{
+			std::cout << evalrpn(infixtorpn(input))<<std::endl;
+		}
+		catch (std::runtime_error)
+		{
+			std::cerr << "The expression input ( " << input <<
+				" ) goes wrong, please recheck it." << std::endl;
+		}
+	}
 }
